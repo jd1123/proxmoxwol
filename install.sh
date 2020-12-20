383b1807217b6c7531533a4e8f7afb65d3efc383
jd1123 <johnnydiabetic@gmail.com>
2020-12-20 17:49:21 -0500

    Few upgrades
    * Daemonized service
    * Added install script
    * Added systemd service file

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 # this is the install script
+sudo cp proxmoxwol-listener.py /usr/bin/
+sudo chmod 744 /usr/bin/proxmoxwol-listener.py
+sudo chown root:root /usr/bin/proxmoxwol-listener.py
+sudo cp proxmoxwol-vm.service /etc/systemd/system/
+sudo chmod 644 /etc/systemd/system/proxmoxwol-vm.service
+sudo chown root:root /etc/systemd/system/proxmoxwol-vm.service
--- proxmoxwol-listener.py
+++ proxmoxwol-listener.py
@@ -5,6 +5,7 @@ import socketserver
 import binascii
 import os
 import daemon
+import daemon.pidfile
 
 class UDPListener(socketserver.BaseRequestHandler):
     def __init__(self, request, client_address, server):
@@ -20,12 +21,23 @@ class UDPListener(socketserver.BaseRequestHandler):
         print('{} wrote:'.format(self.client_address[0]))
         packet = self.parse_packet(data)
         self.getfilenames()
-        print(self.d)
-        if packet[0].upper() == b'FFFFFFFFFFFF':
+        if self.iswol(data):
+            print('WOL Packet found')
             if packet[1].upper() in self.d.keys():
                 print("need to wake up", packet[1].upper())
                 self.wakemachine(self.d[packet[1].upper()])
 
+    def iswol(self, dat):
+        if len(dat) != 102:
+            return False
+        if self.parse_packet(dat)[0].upper() != b'FFFFFFFFFFFF':
+            return False
+        newdat = binascii.hexlify(dat)[12:]
+        n = 12
+        packs = [newdat[i:i+n] for i in range(0, len(newdat), n)]
+        if len(packs) != 16:
+            return False
+        return True
 
     def parse_packet(self, dat):
         return [binascii.hexlify(dat)[:12], binascii.hexlify(dat)[12:24]]
@@ -37,19 +49,38 @@ class UDPListener(socketserver.BaseRequestHandler):
         os.system(self.resume_command + qemu_id)
 
     def parsefiles(self, filename):
-        with open(self.configdir + filename, 'r') as f:
-            for line in f:
-                if line[:4] == 'net0':
-                    mac = line.split('=')[1].split(',')[0]
-                    self.d[self.convertmac(mac.upper())] = filename.split('.')[0]
+        try:
+            with open(self.configdir + filename, 'r') as f:
+                for line in f:
+                    if line[:4] == 'net0':
+                        mac = line.split('=')[1].split(',')[0]
+                        self.d[self.convertmac(mac.upper())] = filename.split('.')[0]
+        except:
+            print('Proxmox configuration files not found at', self.configdir)
 
     def getfilenames(self):
-        files = os.listdir(self.configdir)
-        for f in files:
-            self.parsefiles(f)
+        try:
+            files = os.listdir(self.configdir)
+            for f in files:
+                self.parsefiles(f)
+        except:
+            print('Proxmox configuration files not found at', self.configdir)
 
 
-if __name__ == '__main__':
+def run_server():
     HOST, PORT = '', 9
     server = socketserver.UDPServer((HOST, PORT), UDPListener)
     server.serve_forever()
+
+
+if __name__ == '__main__':
+    daem = False
+    context = daemon.DaemonContext(
+            pidfile = daemon.pidfile.PIDLockFile('/tmp/proxmoxwol-listener.pid'),
+            )
+
+    if daem:
+        with context:
+            run_server()
+    else:
+        run_server()
